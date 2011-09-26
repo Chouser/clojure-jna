0e81174ed271b76899e4cd74c6e88b8c354bdba7
Jan Rychter <jan@rychter.com>
2011-09-26 09:39:35 +0200

    add a minimal project.clj with JNA dependencies

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-jna "1.0.0"
+  :description "Clojure JNA"
+  :dependencies [[net.java.dev.jna/jna "3.3.0"]
+                 [net.java.dev.jna/jna "3.3.0" :classifier "platform"]])
